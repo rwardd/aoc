a7d7f7d1d12b0ec10d06302c2efc900f837a36ce
Ryan Ward <rwardd@outlook.com.au>
2025-12-03 22:58:17 +1030

    feat: day 2 part 2

--- 2025/day2/src/main.rs
+++ 2025/day2/src/main.rs
@@ -17,6 +17,38 @@ fn part1(input: &[(u64, u64)]) -> u64 {
         .sum()
 }
 
+fn is_valid(input: &str) -> bool {
+    let len: usize = input.len() / 2;
+    for i in 1..=len {
+        let pattern = &input[0..i];
+        let mut found = true;
+        for j in (0..input.len()).step_by(pattern.len()) {
+            if !input.len().is_multiple_of(pattern.len()) {
+                found = false;
+                continue;
+            }
+
+            let current = &input[j..j + pattern.len()];
+            if *current != *pattern {
+                found = false;
+            }
+        }
+
+        if found {
+            return found;
+        }
+    }
+
+    false
+}
+
+fn part2(input: &[(u64, u64)]) -> u64 {
+    input
+        .iter()
+        .flat_map(|a| (a.0..=a.1).filter(|id| is_valid(&id.to_string())))
+        .sum()
+}
+
 fn main() -> anyhow::Result<()> {
     let input = "./src/input.txt";
 
@@ -27,7 +59,8 @@ fn main() -> anyhow::Result<()> {
         .filter_map(|mut range| Some((range.next()?.parse().ok()?, range.next()?.parse().ok()?)))
         .collect();
 
-    println!("part one: {}", part1(&part1_input));
+    println!("part one: {}", part1(&part1_input.clone()));
+    println!("part two: {}", part2(&part1_input));
 
     Ok(())
 }
---
+++ 2025/readme.md
@@ -0,0 +1,3 @@
+# AoC 2025
+
+todo: use workspaces instead of creating package for each day
